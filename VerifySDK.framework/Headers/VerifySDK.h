4ba0f33944f4b4acd79b8704166be1af6bf024cc
Priyesh Jain <pjain@telesign.com>
2017-06-07 15:44:06 -0700

    iOS App Verify release v1.0

---
+++ VerifySDK.framework/Headers/VerifySDK.h
@@ -0,0 +1,19 @@
+//
+//  VerifySDK.h
+//  VerifySDK
+//
+//  Created by Krishna Jagadish on 1/9/17.
+//  Copyright © 2017 TeleSign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VerifySDK.
+FOUNDATION_EXPORT double VerifySDKVersionNumber;
+
+//! Project version string for VerifySDK.
+FOUNDATION_EXPORT const unsigned char VerifySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerifySDK/PublicHeader.h>
+
+
